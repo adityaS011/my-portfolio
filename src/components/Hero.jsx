25d3b35853a5b6bfdfb12b26680e37bac06547f2
unknown <aditya.singh.lko2002@bbdec.ac.in>
2023-07-30 20:23:50 +0530

    Your commit message here

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,11 +23,6 @@ const Hero = () => {
       <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
-        <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
-        </div>
-
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className='text-[#915EFF]'>Aditya</span>
@@ -37,18 +32,23 @@ const Hero = () => {
             dynamic websites.
           </p>
         </div>
+
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        </div>
       </div>
 
       {/* Show ComputersCanvas on larger devices */}
       {!isSmallScreen && <ComputersCanvas />}
 
-      {/* Show the image on small devices */}
+      {/* Show the image below the text on small devices */}
       {isSmallScreen && (
-        <div className='flex justify-center items-center mt-8'>
+        <div className='flex flex-col justify-center items-center mt-5'>
           <img
             src='https://cdn3d.iconscout.com/3d/premium/thumb/web-developer-4506461-3738664.png'
             alt='Web Developer'
-            className='w-1/2 sm:w-1/4'
+            className='w-1/2 sm:w-1/4 mt-5'
           />
         </div>
       )}
