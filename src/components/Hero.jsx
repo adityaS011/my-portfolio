46ec59cb83351671bd3ab669cc1f4e2b2e175a84
unknown <aditya.singh.lko2002@bbdec.ac.in>
2023-07-30 20:07:26 +0530

    Your commit message here

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,8 +39,21 @@ const Hero = () => {
         </div>
       </div>
 
-      {!isSmallScreen && <ComputersCanvas />} {/* Render ComputersCanvas only on non-small devices */}
+      {isSmallScreen ? (
+        <div className='flex justify-center items-center'>
+          <img
+            src='https://cdn3d.iconscout.com/3d/premium/thumb/web-developer-4506461-3738664.png'
+            alt='Web Developer'
+            className='w-1/2 sm:w-1/4 mx-auto mt-8'
+          />
+        </div>
+      ) : (
+        <ComputersCanvas />
+      )}
+<<<<<<< Updated upstream
+=======
 
+>>>>>>> Stashed changes
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
