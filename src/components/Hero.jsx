8cdcf296d80fdcd3f50f54874553b62690d08c1e
unknown <aditya.singh.lko2002@bbdec.ac.in>
2023-07-30 20:11:24 +0530

    Your commit message here

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -34,26 +34,25 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop static and <br className='sm:block hidden' />
-              dynamic websites.
+            dynamic websites.
           </p>
         </div>
       </div>
 
-      {isSmallScreen ? (
-        <div className='flex justify-center items-center'>
+      {/* Show ComputersCanvas on larger devices */}
+      {!isSmallScreen && <ComputersCanvas />}
+
+      {/* Show the image on small devices */}
+      {isSmallScreen && (
+        <div className='flex justify-center items-center mt-8'>
           <img
             src='https://cdn3d.iconscout.com/3d/premium/thumb/web-developer-4506461-3738664.png'
             alt='Web Developer'
-            className='w-1/2 sm:w-1/4 mx-auto mt-8'
+            className='w-1/2 sm:w-1/4'
           />
         </div>
-      ) : (
-        <ComputersCanvas />
       )}
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
@@ -64,7 +63,7 @@ const Hero = () => {
               transition={{
                 duration: 1.5,
                 repeat: Infinity,
-                repeatType: "loop",
+                repeatType: 'loop',
               }}
               className='w-3 h-3 rounded-full bg-secondary mb-1'
             />
