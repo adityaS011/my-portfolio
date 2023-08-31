3656d659916822ce72c5143ce061eff9d2a88b7e
Aditya Singh <93607531+adityaS011@users.noreply.github.com>
2023-08-31 15:34:07 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -123,7 +123,7 @@ const public_key = "UZBYWlsxmFADkV1bJ"
           <button
             type='submit'
             className='bg-tertiary   font-medium py-3 px-8 rounded-xl outline-none w-fit text-secondary font-bold shadow-md shadow-primary'
-            onClick={() => window.open("https://drive.google.com/file/d/17Wn7Qhh-uNsQCvtbU_XBqDODb6NYuvxR/view?usp=drivesdk", "_blank")}
+            onClick={() => window.open("https://drive.google.com/file/d/1xD_03hfzedmBBqPuU91k05IJ9WUa5wB9/view?usp=drive_link", "_blank")}
           >
              
            {"My Resume"}
