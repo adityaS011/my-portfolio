615de2556d6adcd03f1bca0adddb49a45dce792d
Aditya Singh <93607531+adityaS011@users.noreply.github.com>
2024-01-09 16:01:53 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -27,6 +27,7 @@ import {
   carrent,
   jobit,
   tripguide,
+  dashboard,
   threejs,
 } from "../assets";
 
@@ -133,32 +134,7 @@ const experiences = [
        "Collaborated with the development team, participating in code reviews and addressing bugs for optimization.",
     ],
   },
-  // {
-  //   title: "Web Developer",
-  //   company_name: "Shopify",
-  //   icon: shopify,
-  //   iconBg: "#383E56",
-  //   date: "Jan 2022 - Jan 2023",
-  //   points: [
-  //     "Developing and maintaining web applications using React.js and other related technologies.",
-  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-  //     "Implementing responsive design and ensuring cross-browser compatibility.",
-  //     "Participating in code reviews and providing constructive feedback to other developers.",
-  //   ],
-  // },
-  // {
-  //   title: "Full stack Developer",
-  //   company_name: "Meta",
-  //   icon: meta,
-  //   iconBg: "#E6DEDD",
-  //   date: "Jan 2023 - Present",
-  //   points: [
-  //     "Developing and maintaining web applications using React.js and other related technologies.",
-  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-  //     "Implementing responsive design and ensuring cross-browser compatibility.",
-  //     "Participating in code reviews and providing constructive feedback to other developers.",
-  //   ],
-  // },
+ 
 ];
 
 const testimonials = [
@@ -235,6 +211,27 @@ const projects = [
     ],
     image: tripguide,
     source_code_link: "https://crpyto-transaction-app.netlify.app/",
+  },
+{
+    name: "Next.js Dasboard App",
+    description:
+      "Developed a Dashboard with dynamic UI elements using React and Next.js for enhanced modularity and maintainability. Leveraged Next.js API Routes for seamless backend integration, facilitating efficient data fetching and handling.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Next.js",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Tailwind CSS",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: dashboard,
+    source_code_link: "https://dashboard-graff.netlify.app/",
   },
   {
     name: "College Chapter Website",
